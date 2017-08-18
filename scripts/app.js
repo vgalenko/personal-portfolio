1de05c7ab516696eb0e3ca4b3530dfb1bd6d4ee5
vgalenko <vitaliygalenko@gmail.com>
2017-08-17 21:48:03 -0700

    touched up a few features on the slider and resized the selectors for mobile view

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 $(function () {
-  setInterval(function(){
-    $('#slider ul').animate({marginLeft:'-800px'}, 800, function(){
-      $('#slider ul li:last').after($('#slider ul li:first'));
-      $(this).css('margin-left', '0');
+  setInterval(function(){ //interval method calls the function continiously
+    $('#slider ul').animate({marginLeft:'-80em'}, 800, function(){  //animate method sets the css property to move img to the left
+      $('#slider ul li:last').after($('#slider ul li:first')); //the function is always looking for the last child, .after()inserts content, specified by the parameter, after each element in the set of matched elements.
+      $(this).css('margin-left', '0'); // img reset
     });
   },3000);
 });
