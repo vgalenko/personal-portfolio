41eb36075a1912301223679d6381d2d8ce2b5734
vgalenko <vitaliygalenko@gmail.com>
2017-08-22 22:08:33 -0700

    added handlebars to skills list

---
+++ scripts/blog.js
@@ -0,0 +1,19 @@
+const skills = {
+  "myskills": [
+    {
+      "skill1": "JavaScript",
+      "skill2": "jQuery",
+      "skill3": "HTML",
+      "skill4": "CSS",
+      "skill5": "RWD",
+      "skill6": "AJAX",
+      "skill7": "SQL"
+    }
+  ]
+};
+
+$(document).ready(function(){
+  let skillsTemplate = $('#template').html();
+  let compileSkillsTemplate = Handlebars.compile(skillsTemplate);
+  $('.skills-list').html(compileSkillsTemplate(skills.myskills[0]));
+});
