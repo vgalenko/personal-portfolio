2143be7b410869253fc7a314be71054c22aeba17
vgalenko <vitaliygalenko@gmail.com>
2017-08-30 23:02:57 -0700

    reconstructed and deleted a few things from server and app

--- public/scripts/views/app.js
+++ public/scripts/views/app.js
@@ -20,28 +20,36 @@ $(document).ready(function(){
   $('.content-display').html(compileTemplate(skills));
 });
 
+$.get('/github/user/repos')
+// .then(success, fail)
+.then(
+  // success
+  data => data.forEach(repo => $('#results').append(`<p>${repo.name}</p>`)),
+  // fail
+  err => console.error(err.status, err.statusText, 'is the way my stuff is broken'));
 
-var app = app || {};
 
-(function(module) {
-  const repos = {};
-
-  repos.all = [];
-
-  repos.requestRepos = function(callback) {
-    // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    //       Don't forget to remove the headers from our request - we're no longer using a token on the
-    //       client side of our app, our new proxyGitHub function will be handling the token using our
-    //       new environment variable!
-    $.get('/github/user/repos')
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    .then(callback);
-  };
-
-  repos.with = attr => repos.all.filter(repo => repo[attr]);
-
-  module.repos = repos;
-})(app);
+// var app = app || {};
+//
+// (function(module) {
+//   const repos = {};
+//
+//   repos.all = [];
+//
+//   repos.requestRepos = function(callback) {
+//     // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
+//     //       Don't forget to remove the headers from our request - we're no longer using a token on the
+//     //       client side of our app, our new proxyGitHub function will be handling the token using our
+//     //       new environment variable!
+//     $.get('/github/user/repos')
+//     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+//     .then(callback);
+//   };
+//
+//   repos.with = attr => repos.all.filter(repo => repo[attr]);
+//
+//   module.repos = repos;
+// })(app);
 
 // $.ajax({
 // 	 url: 'https://api.github.com/user/repos',
